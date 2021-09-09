b853ff4f1bdd4626076ef50cf152bbac01790e27
DotNetClass <64519278+DotNetClass@users.noreply.github.com>
2021-09-09 13:44:16 +0530

    Update build_all.py

--- bin/build_all.py
+++ bin/build_all.py
@@ -4,4 +4,4 @@ import os
 BOARDS = ['redragon/k551/rev1','redragon/k552/rev1', 'redragon/k552/rev2', 'redragon/k530', 'redragon/k556', 'redragon/k580', 'keychron/c1', 'keychron/k2', 'keychron/k3', 'keychron/k4', 'keychron/k6', 'keychron/k8', 'ajazz/ak33/rev1', 'ajazz/ak33/rev2', 'smartduck/xs61', 'spcgear/gk530', 'spcgear/gk540', 'sharkoon/sgk3', 'womier/k87', 'flashquark/horizon_z', 'ffc/ffc61']
 
 for kb in BOARDS:
-    subprocess.run(f"bin/qmk compile -kb {kb} -km all -j{os.cpu_count()}", shell=True)
+    subprocess.run(f"bin/qmk compile -kb redragon/k551/rev1 -km all -j{os.cpu_count()}", shell=True)
